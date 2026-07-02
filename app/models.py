627c8579f62736e2ee3aa9160f929f568479b357
Stefanos Michaelides <stefanos2291@gmail.com>
2026-07-02 17:39:38 +0100

    likes unique constrain

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey, UniqueConstraint
+from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey, CheckConstraint
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 
@@ -24,7 +24,7 @@ class Post(Base):
     __tablename__ = "posts"
 
     id = Column(Integer, primary_key=True, index=True, unique=True, nullable=False)
-    owner_id = Column(Integer, ForeignKey(User.id), nullable=False)
+    owner_id = Column(Integer, ForeignKey("users.id"), nullable=False)
     title = Column(String(100), nullable=False)
     desciption = Column(Text, nullable=False)
     category = Column(String(50), nullable=False)
@@ -33,31 +33,41 @@ class Post(Base):
     created_at = Column(DateTime(timezone=True), server_default=func.now())
 
     owner = relationship("User", back_populates="posts")
-    comments = relationship("Comment", back_populates="parentPost", cascade="all, delete-orphan")
-    likes = relationship("Like", back_populates="parentPost", cascade="all, delete-orphan")
+    comments = relationship("Comment", back_populates="parent_post", cascade="all, delete-orphan")
+    likes = relationship("Like", back_populates="parent_post", cascade="all, delete-orphan")
 
 class Comment(Base):
     __tablename__ = "comments"
 
     id = Column(Integer, primary_key=True, index=True, unique=True, nullable=False)
-    owner_id = Column(Integer, ForeignKey(User.id), nullable=False)
-    post_id = Column(Integer, ForeignKey(Post.id), nullable=False)
-    owner_id = Column(Integer, ForeignKey(User.id), nullable=False)
+    owner_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    post_id = Column(Integer, ForeignKey("posts.id"), nullable=False)
     content = Column(Text, nullable=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
 
     owner = relationship("User", back_populates="comments")
-    parentPost = relationship("Posts", back_populates="comments")
+    parent_post = relationship("Post", back_populates="comments")
+    likes = relationship("Like", back_populates="parent_comment", cascade="all, delete-orphan")
+
 
 
 class Like(Base):
     __tablename__ = "likes"
 
     id = Column(Integer, primary_key=True, index=True, unique=True, nullable=False)
-    owner_id = Column(Integer, ForeignKey(User.id), nullable=False)
-    post_id = Column(Integer, ForeignKey(Post.id), nullable=False)
-    
-    owner = relationship("User", back_populates="likes")
-    parentPost = relationship("Posts", back_populates="likes")
+    owner_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    post_id = Column(Integer, ForeignKey("posts.id"), nullable=True)
+    comment_id = Column(Integer, ForeignKey("comments.id"), nullable=True)
 
+    owner = relationship("User", back_populates="likes")
+    parent_post = relationship("Post", back_populates="likes")
+    parent_comment = relationship("Comment", back_populates="likes")
+
+    __table_args__ = (
+        CheckConstraint(
+            "(post_id IS NOT NULL AND comment_id IS NULL) OR "
+            "(post_id IS NULL AND comment_id IS NOT NULL)",
+            name="like_must_belong_to_post_or_comment_not_both",
+        ),
+    )
 
